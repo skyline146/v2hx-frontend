d13772bda0e4e0ca33ff45b580124bc54d54b50c
Dmitriy Bukatsel <dimasgrand16@gmail.com>
2023-11-26 16:43:22 +0300

    fix: import correct IUserRow type

--- src/store/users.ts
+++ src/store/users.ts
@@ -1,25 +1,25 @@
 import { create } from "zustand";
-import { UserRow } from "shared/lib/types";
+import { IUserRow } from "shared/lib/types";
 // import { produce } from "immer";
 
 interface UserState {
-  users: UserRow[];
+  users: IUserRow[];
   total: number;
 }
 
 type Action = {
-  setUsers: (users: UserRow[]) => void;
+  setUsers: (users: IUserRow[]) => void;
   setTotal: (total: number) => void;
-  updateUser: (data: UserRow) => void;
+  updateUser: (data: IUserRow) => void;
 };
 
 // Create your store, which includes both state and (optionally) actions
 export const useUsersStore = create<UserState & Action>((set) => ({
   users: [],
   total: 0,
-  setUsers: (users: UserRow[]) => set(() => ({ users })),
+  setUsers: (users: IUserRow[]) => set(() => ({ users })),
   setTotal: (total: number) => set(() => ({ total })),
-  updateUser: (data: UserRow) =>
+  updateUser: (data: IUserRow) =>
     set((state) => ({
       users: state.users.map((user) => (user.username === data.username ? data : user)),
     })),
