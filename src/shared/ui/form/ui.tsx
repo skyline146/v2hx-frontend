c14cb1066cb43bdfc81f6fae54b70efcac98569a
Dmitriy Bukatsel <dimasgrand16@gmail.com>
2023-11-29 14:26:14 +0300

    fix: info form view on mobile

--- src/shared/ui/form/ui.tsx
+++ src/shared/ui/form/ui.tsx
@@ -6,21 +6,22 @@ interface IFormProps {
   onSave: () => void;
   loading?: boolean;
   closeModal?: () => void;
-  w?: number;
+  w?: number | string;
   gap?: string;
 }
 
-export const Form: FC<IFormProps> = ({ children, onSave, loading }: IFormProps) => {
+export const Form: FC<IFormProps> = ({ children, onSave, loading, w, gap }: IFormProps) => {
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
         onSave();
       }}
+      style={{ width: w }}
     >
-      <Flex align="center" direction="column">
+      <Flex w={w} gap={gap} direction="column">
         {children}
-        <Box mt={15} w={150}>
+        <Box mt={15} w={150} style={{ alignSelf: "center" }}>
           <Button loading={loading} type="submit" w="100%">
             Save
           </Button>
--- src/widgets/cheat-info-form/cheat-info-form.tsx
+++ src/widgets/cheat-info-form/cheat-info-form.tsx
@@ -1,5 +1,6 @@
-import { Flex, Select, TextInput } from "@mantine/core";
+import { Flex, Select, TextInput, em } from "@mantine/core";
 import { useForm } from "@mantine/form";
+import { useMediaQuery } from "@mantine/hooks";
 import { useEffect, useState } from "react";
 
 import { notification } from "shared/lib";
@@ -8,6 +9,8 @@ import { Form } from "shared/ui";
 
 export const CheatInfoForm = () => {
   const [loading, setLoading] = useState(false);
+
+  const isMobile = useMediaQuery(`(max-width: ${em(500)})`);
 
   const infoForm = useForm({
     initialValues: {
@@ -24,6 +27,7 @@ export const CheatInfoForm = () => {
   return (
     <Flex w="100%" justify="center">
       <Form
+        w={isMobile ? "100%" : 400}
         gap="xs"
         loading={loading}
         onSave={() => {
@@ -39,20 +43,14 @@ export const CheatInfoForm = () => {
             .then(() => setLoading(false));
         }}
       >
-        <Flex w={400} gap="xs" direction="column">
-          <Select
-            label="Status"
-            size="md"
-            data={["On update", "Available", "Use at own risk"]}
-            {...infoForm.getInputProps("status")}
-          />
-          <TextInput label="Cheat version" size="md" {...infoForm.getInputProps("cheat_version")} />
-          <TextInput
-            label="Loader version"
-            size="md"
-            {...infoForm.getInputProps("loader_version")}
-          />
-        </Flex>
+        <Select
+          label="Status"
+          size="md"
+          data={["On update", "Available", "Use at own risk"]}
+          {...infoForm.getInputProps("status")}
+        />
+        <TextInput label="Cheat version" size="md" {...infoForm.getInputProps("cheat_version")} />
+        <TextInput label="Loader version" size="md" {...infoForm.getInputProps("loader_version")} />
       </Form>
     </Flex>
   );
