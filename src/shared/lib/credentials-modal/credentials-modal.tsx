40687fec48591589e963755b8b778e9aa1eea2f3
Dmitriy Bukatsel <dimasgrand16@gmail.com>
2024-01-02 15:00:56 +0300

    feat: copy important with account credentials

--- src/shared/lib/credentials-modal/credentials-modal.tsx
+++ src/shared/lib/credentials-modal/credentials-modal.tsx
@@ -1,9 +1,11 @@
 import { Flex, CopyButton, Button, Text } from "@mantine/core";
 import { modals } from "@mantine/modals";
 
+import textToCopy from "./text-to-copy";
+
 export const credentialsModal = (username: string, password: string) =>
   modals.open({
-    title: "New user credentials",
+    title: "New Account",
     centered: true,
     children: (
       <Flex direction="column" gap="sm">
@@ -13,7 +15,7 @@ export const credentialsModal = (username: string, password: string) =>
         <Text fw={700} variant="gradient" gradient={{ from: "blue", to: "cyan", deg: 90 }}>
           Password: {password}
         </Text>
-        <CopyButton value={`Username: ${username}\nPassword: ${password}`}>
+        <CopyButton value={textToCopy(username, password)}>
           {({ copied, copy }) => (
             <Button color={copied ? "teal" : "blue"} onClick={copy}>
               {copied ? "Copied!" : "Copy to clipboard"}
---
+++ src/shared/lib/credentials-modal/text-to-copy.ts
@@ -0,0 +1,19 @@
+const textToCopy = (username: string, password: string) => {
+  return `Username: ${username}
+Password: ${password}
+-----------------------------
+You can change your credentials here and download loader: https://v2hx.pro/
+-----------------------------
+You irrevocably accepting our Terms of Use by first login to your account.
+English: https://telegra.ph/V2HX-Terms-of-Use-12-24
+Russian: https://telegra.ph/V2HX-Usloviya-ispolzovaniya-12-24
+-----------------------------
+How to start playing: 
+English: https://telegra.ph/V2HX-How-to-start-playing-12-24
+Russian: https://telegra.ph/V2HX-Kak-nachat-igrat-12-24
+-----------------------------
+If there any problems with crashes, after the crash, open the loader and click on the “send crash log” button so we can solve problems faster.
+If you have any problems/questions, you can create ticket here: https://discord.gg/8bDf3BdbYd`;
+};
+
+export default textToCopy;
